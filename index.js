9d001bfe311f035cc738fa6e6c0d01160566d2ea
Miles Marillo <mjm03@hampshire.edu>
2019-06-04 14:12:06 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+'use strict';
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+String.prototype.titleize = function() {
+    var string_array = this.split(' ');
+    string_array = string_array.map(function(str) {
+       return str.capitalize(); 
+    });
+    
+    return string_array.join(' ');
+}
+
+function titleCased() {
+  return tutorials.map(element => element.titleize())
+}
+
